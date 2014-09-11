1a4fde083944bccda2f93aa66b19cae2eb5c23fc
Chiradip Mandal <chiradip@chiradip.com>
2014-09-11 11:22:41 -0700

    name

--- python/confirmemail.py
+++ python/confirmemail.py
@@ -14,12 +14,13 @@ p = r.pubsub()
 p.subscribe("RegReqConfEmail")
 
 def sendmail(json_msg):
-    # try:
-    #     name = json_msg['name']
-    # except:
-    #     name = "There"
+    name = "There"
+    try:
+        name = json_msg['name']
+    except:
+        name = "There"
 
-    msg = MIMEText("Hello There!!! Thanks for the registration request. Check for your email for further actions. It may take longer due to system load and verification.")
+    msg = MIMEText("Hello ", name, "!!! Thanks for the registration request. Check your email for further actions. It may take longer due to system load and verification.")
     msg['Subject'] = "Paperless CLub registration request confirmation message"
     msg['To'] = json_msg['email']
     msg['Bcc'] = "chiradip@chiradip.com"
