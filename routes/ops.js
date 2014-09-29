47973ab92cdae4e3f442eb99f5020b40f0204bbd
Chiradip Mandal <chiradip@chiradip.com>
2014-09-29 16:06:14 -0700

    chnage

--- routes/ops.js
+++ routes/ops.js
@@ -47,7 +47,7 @@
   };
 
   dbentry = function(req) {
-    console.log("Req: " + req);
+    console.log("Req: " + (util.inspect(req)));
     return filedata.getData(function(err, data) {
       if (!err) {
         console.log("Data: " + data);
